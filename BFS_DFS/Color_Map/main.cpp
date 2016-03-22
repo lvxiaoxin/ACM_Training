5276f33f6a8ba6afea24d8775a8b172f3937011b
lvxiaoxin <lvxin1204@163.com>
2016-03-22 13:34:38 +0800

    color map ok

--- BFS_DFS/Color_Map/main.cpp
+++ BFS_DFS/Color_Map/main.cpp
@@ -1,8 +1,10 @@
 #include <cstdio>
 #include <cstring>
+#include <algorithm>
+using namespace std;
 
-int N;
-bool g[30] = {false};
+int N, color=0;
+int g[30] = {0};
 bool M[30][30] = {false};
 
 int translate(char C)
@@ -10,20 +12,37 @@ int translate(char C)
     return C-64;
 }
 
-void dfs()
+void dfs(int it)
 {
-
+    int j = 0;
+    int i = 0;
+    for(i=0; i<4; ++i)
+    {
+        g[it] = i;
+        for (j = 1; j <= N; ++j)
+        {
+            if (M[it][j] == true && g[it] == g[j])
+                break;
+        }
+        if (j == N+1) break;
+    }
+    i++;
+    color = max(i, color);
+    if(it==N) return;
+    dfs(++it);
 }
+
 
 int main()
 {
     scanf("%d", &N);
     while(N!=0)
     {
+        color = 0;
         getchar();
         char C, D;
         memset(M, false, sizeof(M));
-        memset(g, false, sizeof(g));
+        memset(g, -1, sizeof(g));
         for(int i=0; i<N; i++)
         {
             scanf("%c%c", &C,&D);
@@ -37,6 +56,8 @@ int main()
                 M[k][m] = true;
             }
         }
+
+        /*
         for(int i=1; i<=N; i++)
          {
            for(int j=1; j<=N; j++)
@@ -47,9 +68,10 @@ int main()
            }
                printf("\n");
          }
-
-
-//            dfs();
+         */
+        dfs(1);
+        if(color==1) printf("1 channel needed.\n");
+        else printf("%d channels needed.\n", color);
         scanf("%d", &N);
     }
     return 0;
