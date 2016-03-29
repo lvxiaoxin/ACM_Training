aa98c1ae3ee7d08c6143ca863d56ea28f533b446
lvxiaoxin <lvxin1204@163.com>
2016-03-29 23:16:26 +0800

    pause

---
+++ bfs_dfs/Maze_2/main.cpp
@@ -0,0 +1,95 @@
+/**
+ *name: Maze_2
+ *p_ID: bnuoj 1055
+ */
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int N, M, x, y, num = 0;
+int dir[][2] = {{-1,0}, {0, 1}};
+char str[101][101];
+bool vis[101][101];
+
+bool easyGo(int x, int y)
+{
+    if(x>=0 && y>=0 && x<N && y<M && str[x][y]!='*' && !vis[x][y])
+        return true;
+    else return false;
+}
+
+void dfs(int x, int y)
+{
+    if(str[x][y]=='T')
+    {
+        num++;
+        num %= 1908;
+        return;
+    }
+    /*
+    for(int i=0; i<2; i++)
+    {
+        int temp_x, temp_y;
+        temp_x = a.x + dir[i][0];
+        temp_y = a.y + dir[i][1];
+   //     cout << temp_x << " " << temp_y << "  temp" << endl;
+        if(easyGo(temp_x, temp_y))
+        {
+            pan next;
+            next.x = temp_x;
+            next.y = temp_y;
+            vis[temp_x][temp_y] = true;
+            dfs(next);
+            vis[temp_x][temp_y] = false;
+        }
+    }
+     */
+
+    if(easyGo(x-1, y))
+    {
+        vis[x-1][y] = true;
+        dfs(x-1, y);
+        vis[x-1][y] = false;
+    }
+    if(easyGo(x, y+1))
+    {
+        vis[x][y+1] = true;
+        dfs(x, y+1);
+        vis[x][y+1] = false;
+    }
+    return;
+}
+
+int main()
+{
+    while(scanf("%d%d", &N, &M)!=EOF)
+    {
+        num = 0;
+        memset(str, '\0', sizeof(str));
+        memset(vis, false, sizeof(vis));
+        int i=0;
+        for(int i=0; i<N; i++)
+            scanf("%s", str[i]);
+        int flag = 1;
+        for(int i=0; i<N; i++)
+        {
+            for(int j=0; j<M; j++)
+            {
+                if(str[i][j]=='S')
+                {
+                    x = i;
+                    y = j;
+                    vis[i][j] = true;
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag==0)
+                break;
+        }
+        dfs(x, y);
+        printf("%d\n", num);
+    }
+}
